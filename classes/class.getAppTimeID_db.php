d75887e0296538d1d1f7f6212244ca55609351b4
galoishelley <shelleymyl@gmail.com>
2016-09-25 23:19:48 +0800

    class.getAppTimeID.php add

---
+++ classes/class.getAppTimeID_db.php
@@ -0,0 +1,31 @@
+<?php 
+include_once('class.database.php');
+
+class getAppTimeID_DB{
+    private $db;
+    private $_dbug;
+    public function __construct(){  
+
+        $this->_dbug = false;
+        $this->db = new Database();
+    }
+
+    public function viewAll($arr_values=0,$requesttype=0,$start=0,$lenght=10){
+        if($this->_dbug){
+            echo "[---viewAll---arr_values]";
+            print_r($arr_values);
+        }
+
+        $sql = "SELECT * FROM `fd_rel_doctor_appointment_time` WHERE `DOCTOR_ID` = ".$arr_values["DOCTOR_ID"]." and `APPOINTMENT_DATE` = str_to_date('".$arr_values["APPOINTMENT_DATE"]."','%d-%m-%Y') and `APPOINTMENT_TIME` = str_to_date('".$arr_values["APPOINTMENT_TIME"]."','%H:%i')  and `ACTIVE_STATUS`=1";
+
+        if($this->_dbug){
+            echo "[---viewAll---sql]";
+            print_r($sql);
+        }
+        // print_r($sql);
+        $ret = $this->db->fetchAll_sql($sql,null);
+        
+        return $ret;
+    }
+}
+?>
