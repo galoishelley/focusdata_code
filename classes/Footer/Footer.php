9ad6f059a05eb181159cb12b9e2a541d8748d826
Xin <yxin1@visteon-jv.com>
2017-04-19 16:42:49 +0800

    Update footer text #137

--- classes/Footer/Footer.php
+++ classes/Footer/Footer.php
@@ -3,7 +3,7 @@
 <div class="row">
 
 <div class="col-lg-12 center">
-<a href="mailto:INFO@DRPAGES.COM.AU" style="color:#0366d6;">INFO@DRPAGES.COM.AU</a>
+<a href="mailto:INFO@DRPAGES.COM.AU" style="color:white;">INFO@DRPAGES.COM.AU</a>
 </div>
 <div class="col-lg-12 center">
 <p class="privacy">&copy; <em id="copyright-year"></em> <i>|</i> Focusdata, Inc.</p>
